1b38fed8a2e7ffef9b3fc24fe309906b243d80e1
luishenrick <l.henrick@live.com>
2021-11-13 22:47:28 -0300

    variavel_Long

---
+++ Long.java
@@ -0,0 +1,16 @@
+
+public class Long {
+	public static void main(String [] args) {
+		long varLong1 = 500000; 
+		long varLong2 = 500000000000009L;		// sem o "L" não é possivel  mostrar variaveis acima de 9 caracteres
+		
+		System.out.println(varLong1);
+		System.out.println(varLong2);
+
+
+		
+	}
+}
+
+
+
