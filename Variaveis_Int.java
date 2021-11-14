618a2a79f3a2cb39865011395915634db3499c63
luishenrick <l.henrick@live.com>
2021-11-13 22:07:15 -0300

    variavel_inteira

---
+++ Variaveis_Int.java
@@ -0,0 +1,16 @@
+
+public class Variaveis_Int {
+	public static void main(String[] args) {
+		
+		int varInt = 100;  // variavel inteira
+		
+		System.out.println("Valor: " + varInt);
+		
+		System.out.println(" Menor valor da variavel: " + Integer.MIN_VALUE);		// mostra o menor valor que consigo colocar nesta variavel inteira
+		
+		System.out.println("Maior valor da variavel: " + Integer.MAX_VALUE);		// mostra o maior que consigo colocar nesta variavel inteira
+		
+		
+	}
+
+}
