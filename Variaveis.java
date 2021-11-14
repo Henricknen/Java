76d0f999a194b5e3750cb7576e8693b69da8cc70
luishenrick <l.henrick@live.com>
2021-11-13 22:03:22 -0300

    menssagem_ola_mundo_

---
+++ Variaveis.java
@@ -0,0 +1,11 @@
+
+public class Variaveis {
+	public static void main(String[] args) {
+		
+		System.out.print(" codificando em java, "); 		// não pula linha para exibir o valor
+		System.out.println("Ola mundo!!!");  			// comando que exibe o valor na tela "println - exibe a menenssagem na outra linha"
+	}
+
+}
+
+	
