6580ca0dc175a0b9c14c24d2d30d2a042c917808
luishenrick <l.henrick@live.com>
2021-11-13 22:09:54 -0300

    atributo_Long

---
+++ Variaveis_Long.java
@@ -0,0 +1,13 @@
+public class Variaveis_Long {
+	public static void main(String[] args) {
+		
+		long varLong1 = 50000;
+		long varLong2 = 1000000000000L;		//  L deixa atribuir um valor grande 
+		
+		System.out.println(varLong1);
+		System.out.println(varLong2);
+
+
+	}
+
+}
