a043fe9c5f937de73c38f644bc386790fef9490a
luishenrick <l.henrick@live.com>
2021-11-14 21:12:32 -0300

    outras variaveis

---
+++ Variaveis_outras_variaveis.java
@@ -0,0 +1,23 @@
+
+public class Variaveis_outras_variaveis {
+	public static void main(String[] args) {
+		System.out.println("Variavel_Boolean");
+		boolean varBoolean = true;		// Boolean 1 bit
+		
+		System.out.println(varBoolean); 
+		
+		
+		System.out.println("Variavel_Char");
+		char simbolo = 'a';
+		System.out.println(simbolo);
+		simbolo = 97;											// vai ter mesmo valor da primeir variavel "a", pois trabalha com a tabela ASCII
+		System.out.println(simbolo);
+		
+		
+		System.out.println("Variavel_String");
+		String palavra = "Luis henrique silva ferreira";			// inicia com o nome da string em maiuscula
+		System.out.println(palavra);
+		
+	}
+
+}
