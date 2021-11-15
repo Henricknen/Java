04eaab32965ac09443863f33dc45a34ad105b7dd
luishenrick <l.henrick@live.com>
2021-11-15 20:04:17 -0300

    operadores_de_relacionais

---
+++ operadoresss.java
@@ -0,0 +1,22 @@
+
+public class operadoresss {
+	public static void main(String[] args) {
+		
+		System.out.println(" 1 é igual 2");
+		System.out.println(1 == 2);		
+		
+		System.out.println("1 é diferente de 2");
+		System.out.println(1 != 2);
+		
+		System.out.println("1 é maior 2");
+		System.out.println(1 > 2);
+		
+		System.out.println("1 é menor 2");
+		System.out.println(1 < 2);
+		
+		System.out.println("1 é maior ou igual a 1");
+		System.out.println(1 >= 1);
+		
+	}
+
+}
