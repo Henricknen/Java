a0a5de6d5185fc815f327851f65751355f5f4f0e
luishenrick <l.henrick@live.com>
2021-11-15 19:44:39 -0300

    operadores_de_atribuição_compostos

---
+++ operadoress.java
@@ -0,0 +1,22 @@
+
+public class operadoress {
+	public static void main(String[] args) {
+		
+		int numero = 10;
+		
+		System.out.println("atribuição_composta");
+		numero += 4;
+		System.out.println(numero);
+		
+		numero -= 7;
+		System.out.println(numero);
+		
+		numero *= 6;
+		System.out.println(numero);
+		
+		numero /= 2;
+		System.out.println(numero);
+		
+	}
+
+}
