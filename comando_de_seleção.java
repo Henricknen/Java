818d43d2e38ff8c75ae72bb5202f79bc27e894da
luishenrick <l.henrick@live.com>
2021-11-16 00:19:10 -0300

    comando_switch

---
+++ comando_de_seleção.java
@@ -0,0 +1,28 @@
+
+public class comando_de_seleção {
+	public static void main(String[] args) {
+		
+		int escolha = 67;
+		switch(escolha) {
+		case 1:
+			System.out.println("Numero 1");
+			break;
+			
+		case 2:
+			System.out.println("Numero 2");
+			break;
+			
+		case 3:
+			System.out.println("Numero 3");
+			break;
+			
+		case 4:
+			System.out.println("Numero 4");
+			break;
+		default:
+			System.out.println("Outro numero");
+		}
+	}
+	
+
+}
