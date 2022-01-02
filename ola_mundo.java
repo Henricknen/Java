912ecb4f354d9f93a80a1847043f4e62f03fff25
Luis Henrique Silva Ferreira <56833636+Henricknen@users.noreply.github.com>
2022-01-02 20:32:34 -0300

    Delete ola_mundo.java

--- ola_mundo.java
+++
@@ -1,6 +0,0 @@
-
-public class ola_mundo {// criando_classe_ola_mundo
-	public static void main(String [] args) {		// metodo_publico,_void_não_retorna_nada
-	System.out.println("Ola_munndo!!");
-	}
-}
