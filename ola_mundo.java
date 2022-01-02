fb7714a8aab6698f8db18934ecac2dfd75791b08
luishenrick <l.henrick@live.com>
2022-01-02 20:27:16 -0300

    imprimindo_na_tela_Ola_mundogit add ola_mundo.java!

---
+++ ola_mundo.java
@@ -0,0 +1,6 @@
+
+public class ola_mundo {// criando_classe_ola_mundo
+	public static void main(String [] args) {		// metodo_publico,_void_não_retorna_nada
+	System.out.println("Ola_munndo!!");
+	}
+}
