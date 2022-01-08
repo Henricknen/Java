623e7158ebd413a0bb5283cc6dcd83ddee2b5b5e
luishenrick <l.henrick@live.com>
2022-01-08 20:46:21 -0300

    criando_classe_e_instanciando_objeto

--- codificando_java/Jogador.java
+++ codificando_java/Jogador.java
@@ -3,6 +3,6 @@ package codificando_java;
 public class Jogador {
 
 	public Jogador(int n) {			// construtor_nao_tem_retorno_e_precisa_ter_o_mesmo_nome_da_classe_/_int_n_metodo_de_entrada
-		System.out.printf("%nJogador numero %d criado%n");
+		System.out.printf("%nJogador numero %d criado%n", n);
 	}
 }
