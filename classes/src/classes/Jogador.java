74e04cbeb792391704191aa5f44b89028cdbd84e
luishenrick <l.henrick@live.com>
2022-01-10 00:19:10 -0300

    criando_classe_e_instanciando_objeto

---
+++ classes/src/classes/Jogador.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Jogador {
+	
+	public Jogador(int n) { // declaracao_de_variavel_inicializada_com_modificador_de-acesso_"public"_pode_ser_acessada_de_fora_da_classe
+		
+		System.out.printf("%njogador numero %d criado%n", n);
+		
+	}
+	
+}
---
+++ classes/src/classes/Princiipal.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Princiipal {
+
+	public static void main(String[] args) {		// programa_principal_tem_o_main
+		
+		Jogador j1 = new Jogador(1);			// criando_objeto_da_classe_jogador
+		Jogador j2 = new Jogador(2);
+		Jogador j3 = new Jogador(3);
+	}
+}
